b5f856deeb4fd29647ae57214d0c77f1597d421c
Idile Waais <idilewaais@gmail.com>
2024-11-12 19:39:01 +0100

    collapse animation

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -6,13 +6,13 @@ import image from '../images/arrow.png';
 
 function Collapse({title, content}) {
     const [visible, setVisible] = useState(false);
-    //Définir le height du contenu du collapse
+
     const contentRef = useRef(null);
     const [height, setHeight] = useState('0px');
     useEffect(() => {
         setHeight(visible ? `${contentRef.current.scrollHeight}px` : '0px');
       }, [visible]);
-    //Utilisation de l'url pour ajouter un underline au bon élément de la nav
+
     const location = useLocation();
     const className = location.pathname.includes('/Apropos') ? 'collapse-container' : 'logement-container';
 
@@ -22,7 +22,7 @@ function Collapse({title, content}) {
                 <p>{title}</p>
                 <img src={image} alt='arrow'  className={visible ?'arrow arrow-up' : 'arrow arrow-down'}></img>
             </button> 
-            {visible && <div  ref={contentRef} className='collapse-content' style={{ height }} key={content.id}>
+            {visible && <div  ref={contentRef} className='collapse-content' style={{ height, transition: 'height 0.3s ease', overflow: 'hidden' }} key={content.id}>
                 <p>{content}</p>
             </div>}
         </div>
